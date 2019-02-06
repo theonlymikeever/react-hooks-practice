d79ab29c50e789d0d391f04284b1a9bdbc6c5134
Mike Kerslake <theonlymikeever@gmail.com>
2019-02-06 10:38:01 -0500

    First!

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const INITIAL_GAME_STATE = { victory: false, startTime: null, endTime: null };
+  const SNIPPETS = [
+    'Bears, beets, battlestar galactica',
+    "What's Forrest Gump's password? 1Forrest1",
+    'Where do programmers like to hangout? The Foo Bar'
+  ];
+  const [snippet, setSnippet] = useState('');
+  const [userText, setUserText] = useState('');
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+
+  const chooseSnippet = snippetIndex => () => {
+    console.log('setSnippet idx', snippetIndex);
+    setSnippet(SNIPPETS[snippetIndex]);
+    setGameState({ ...gameState, startTime: new Date().getTime() });
+  };
+
+  const updateUserText = e => {
+    setUserText(e.target.value);
+
+    if (e.target.value === snippet) {
+      setGameState({
+        ...gameState,
+        victory: true,
+        endTime: new Date().getTime() - gameState.startTime
+      });
+    }
+  };
+
+  return (
+    <div className="App">
+      <h2>Type Race</h2>
+      <hr />
+      {snippet}
+      <h4>{gameState.victory ? `Done! 🎉  Time: ${gameState.endTime}ms` : null}</h4>
+      <input value={userText} onChange={updateUserText} />
+      <hr />
+      {SNIPPETS.map((SNIPPET, idx) => (
+        <button onClick={chooseSnippet(idx)} key={idx}>
+          {SNIPPET.substring(0, 10)}...
+        </button>
+      ))}
+    </div>
+  );
+};
 
 export default App;
